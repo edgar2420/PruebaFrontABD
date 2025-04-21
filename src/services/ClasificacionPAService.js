9445f3154a479ec79d9602c5e1637c4b9a34d4ff
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-04-21 17:13:07 -0400

    Update ClasificacionPAService.js

--- src/services/ClasificacionPAService.js
+++ src/services/ClasificacionPAService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api.render.com/deploy/srv-d03atc0dl3ps73fim78g?key=4IBMgwAoeFQ/clasificacion-pa";
+const API_URL = "https://pruebasubidabd.onrender.com/clasificacion-pa";
 
 const getToken = () => localStorage.getItem("token");
 
