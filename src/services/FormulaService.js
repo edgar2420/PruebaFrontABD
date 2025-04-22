fa647188ef460193fc6661f272acfbaffb7d36ad
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-04-22 08:55:10 -0400

    Update FormulaService.js

--- src/services/FormulaService.js
+++ src/services/FormulaService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://api.render.com/deploy/srv-d03atc0dl3ps73fim78g?key=4IBMgwAoeFQ/formulas";
+const API_URL = "https://pruebasubidabd.onrender.com/formulas";
 
 export const getFormulasPorProducto = async (nombre) => {
     const response = await axios.get(`https://pruebasubidabd.onrender.com/formulas/producto/${encodeURIComponent(nombre)}`);
