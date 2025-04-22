9730684b1bd5d4b18b73f4ccc5560f7066763697
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-04-22 08:56:48 -0400

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,7 +14,7 @@ const FormLogin = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://pruebasubidabd.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nombre, password }),
